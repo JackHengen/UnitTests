015444fa64a98fc5e1da222a185b930b2aa87812
sen5217 <sen5217@nyu.edu>
2025-10-26 01:50:05 -0400

    Commiting javascript frameworks research

---
+++ javascript/add.js
@@ -0,0 +1,9 @@
+function addTrue(a, b) { 
+    return a + b;
+}
+
+function addFalse(a, b) { 
+    return a - b;
+}
+
+module.exports = { addTrue, addFalse };
---
+++ javascript/mocha/.mocharc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    require: ['chai/register-expect'],
+    recursive: true,
+    timeout: 4000,
+    spec: ['add.test.js'],
+    reporter: 'dot',
+  };
